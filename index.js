5d65c6dc01a9934981f44654a57a905f97c31fac
pbielinux <pedrobielik@hotmail.com>
2021-11-01 18:35:40 +0000

    email server

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-// ---
-const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
-const smallMenu = document.querySelector('.header__sm-menu')
-const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
-const headerHamMenuCloseBtn = document.querySelector(
-  '.header__main-ham-menu-close'
-)
-const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+//get the form by its id
+const form = document.getElementById("contact-form"); 
 
-hamMenuBtn.addEventListener('click', () => {
-  if (smallMenu.classList.contains('header__sm-menu--active')) {
-    smallMenu.classList.remove('header__sm-menu--active')
-  } else {
-    smallMenu.classList.add('header__sm-menu--active')
-  }
-  if (headerHamMenuBtn.classList.contains('d-none')) {
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
-  } else {
-    headerHamMenuBtn.classList.add('d-none')
-    headerHamMenuCloseBtn.classList.remove('d-none')
-  }
-})
-
-for (let i = 0; i < headerSmallMenuLinks.length; i++) {
-  headerSmallMenuLinks[i].addEventListener('click', () => {
-    smallMenu.classList.remove('header__sm-menu--active')
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
-  })
-}
-
-// ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
-
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
-})
+const sendMail = (mail) => {
+	//1.
+	fetch("http://localhost:5000/contact", {
+	  method: "POST", //2.
+	  body: mail, //3.
+	}).then((response) => {
+	  return response.json();
+	});
+  };
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //2.
+  let mail = new FormData(form);
+  //3.
+  sendMail(mail);
+});
